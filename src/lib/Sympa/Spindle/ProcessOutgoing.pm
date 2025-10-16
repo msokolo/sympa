e23b25c77b7b3710cd34daf0359d63dcb36135de
msokolo <mariusz.sokolowski@uj.edu.pl>
2025-10-16 13:47:21 +0200

    Rozpoznanie albo testowanie 21

--- src/lib/Sympa/Spindle/ProcessOutgoing.pm
+++ src/lib/Sympa/Spindle/ProcessOutgoing.pm
@@ -381,12 +381,16 @@ sub _twist {
         if %arc
         or $message->{shelved}{dkim_sign};
 
-    $log->syslog('err', 'tujestem dkim d %s',$dkimi(d));
-    $log->syslog('err', 'tujestem dkim i %s',$dkimi(i));
-    $log->syslog('err', 'tujestem dkim s %s',$dkimi(s));
-    $log->syslog('err', 'tujestem arc  d %s',$arc(d));
-    $log->syslog('err', 'tujestem arc  i %s',$arc(i));
-    $log->syslog('err', 'tujestem arc  s %s',$arc(s));
+    if ( %dkim ) {
+        $log->syslog('err', 'tujestem dkim d %s',$dkimi(d));
+        $log->syslog('err', 'tujestem dkim i %s',$dkimi(i));
+        $log->syslog('err', 'tujestem dkim s %s',$dkimi(s));
+    }
+    if ( %arc ) {
+        $log->syslog('err', 'tujestem arc  d %s',$arc(d));
+        $log->syslog('err', 'tujestem arc  i %s',$arc(i));
+        $log->syslog('err', 'tujestem arc  s %s',$arc(s));
+    }
     $log->syslog('err', 'tujestem rm_sig %s',$rm_sig);
 #    $log->syslog('err', 'tujestem merge %s',$message->{shelved}{merge});
 #    $log->syslog('err', 'tujestem smime_encrypt %s',$message->{shelved}{smime_encrypt});
