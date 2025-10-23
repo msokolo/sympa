acfe356f0ac707fb7313b53d680429ba78ae5790
msokolo <mariusz.sokolowski@uj.edu.pl>
2025-10-23 10:32:34 +0200

    Rozpoznanie albo testowanie 37

--- src/lib/Sympa/Spindle.pm
+++ src/lib/Sympa/Spindle.pm
@@ -87,7 +87,7 @@ sub spin {
             use Sympa::Message;
             $log->syslog('err', 'tujestem message: %s',ref($message));
             my $tujemesa = $message->dup();
-            $log->syslog('err', 'tujestem message encoding: %s',$tujemesa->get_header('Content-Transfer-Encoding',));
+#            $log->syslog('err', 'tujestem message encoding: %s',$tujemesa->get_header('Content-Transfer-Encoding',));
             my $status = $self->_twist($message);
             # If the result is arrayref, splice to the classes in it.
             while (ref $status eq 'ARRAY' and @$status) {
